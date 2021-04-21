639fdbaf52e5ce381dda9f76992d8e2d6b7c224d
weat0212 <weat0212@gmail.com>
2021-04-21 15:10:22 +0800

    Message Converter Config

---
+++ src/main/java/com/example/jmsspring/config/JmsConfig.java
@@ -0,0 +1,27 @@
+package com.example.jmsspring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author I-Chung, Wang
+ * @date 2021/4/21 下午 03:02
+ */
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
