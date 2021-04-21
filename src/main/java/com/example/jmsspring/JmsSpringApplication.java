2b34d2240cfb6e4f8a462adeb18bbffffefcc4ba
weat0212 <weat0212@gmail.com>
2021-04-21 14:19:51 +0800

    first commit

---
+++ src/main/java/com/example/jmsspring/JmsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jmsspring/JmsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
