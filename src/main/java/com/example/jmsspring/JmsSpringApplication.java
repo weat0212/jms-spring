d6e731244a2bf11935f753eb8ca0cfbc37e6255e
weat0212 <weat0212@gmail.com>
2021-04-23 15:48:45 +0800

    ActiveMQ docker

--- src/main/java/com/example/jmsspring/JmsSpringApplication.java
+++ src/main/java/com/example/jmsspring/JmsSpringApplication.java
@@ -1,8 +1,8 @@
 package com.example.jmsspring;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
+//import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+//import org.apache.activemq.artemis.core.server.ActiveMQServer;
+//import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +11,13 @@ public class JmsSpringApplication {
 
     public static void main(String[] args) throws Exception {
 
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0"));
-
-        server.start();
+//        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//                .setPersistenceEnabled(false)
+//                .setJournalDirectory("target/data/journal")
+//                .setSecurityEnabled(false)
+//                .addAcceptorConfiguration("invm", "vm://0"));
+//
+//        server.start();
 
         SpringApplication.run(JmsSpringApplication.class, args);
     }
